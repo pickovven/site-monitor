bfe66371358d07aaf3ae38cadbf7ede8c045f788
pickovven <owen.pickford@gmail.com>
2021-11-21 21:01:34 -0800

    Make the port a variable

--- app.ts
+++ app.ts
@@ -3,20 +3,39 @@ import axios from 'axios';
 import express from 'express';
 import commandRouter from './api/controllers/command.js'
 
-
 const server = express()
 server.use(express.json());
 const slackToken = process.env.SLACKBOT_TOKEN;
 server.use('/api/commands', commandRouter)
 
-async function run() {
-  const url = 'https://hooks.slack.com/services/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';
-  const res = await axios.post(url, {
-    channel: '#test',
-    text: 'Hello Pam! I\'m a slackbot'
-  }, { headers: { authorization: `Bearer ${slackToken}` } });
+const hostname = '127.0.0.1';
 
-  console.log('Done', res.data);
+let port: number 
+if(process.env.PORT) {
+	port = parseInt(process.env.PORT)
+}
+else {
+	port = 3000;
+}  
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+async function run() {
+  var urls = ['https://theurbanist.org'];
+	if (urls) {
+		urls.forEach(async function(value) {
+			const res = await axios.get(value);
+			if(res.status != 200) {
+				const url = 'https://hooks.slack.com/services/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';
+				const res = await axios.post(url, {
+					channel: '#test',
+					text: `${value} is down`
+				}, { headers: { authorization: `Bearer ${slackToken}` } });
+			}
+		});
+		
+	}
 }
 
 run()
--- dist/app.js
+++ dist/app.js
@@ -18,21 +18,36 @@ const express_1 = __importDefault(require("express"));
 const command_js_1 = __importDefault(require("./api/controllers/command.js"));
 const server = (0, express_1.default)();
 server.use(express_1.default.json());
-const hostname = '127.0.0.1';
-const port = 3000;
 const slackToken = process.env.SLACKBOT_TOKEN;
 server.use('/api/commands', command_js_1.default);
+const hostname = '127.0.0.1';
+let port;
+if (process.env.PORT) {
+    port = parseInt(process.env.PORT);
+}
+else {
+    port = 3000;
+}
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        const url = 'https://hooks.slack.com/services/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';
-        const res = yield axios_1.default.post(url, {
-            channel: '#test',
-            text: 'Hello Pam! I\'m a slackbot'
-        }, { headers: { authorization: `Bearer ${slackToken}` } });
-        console.log('Done', res.data);
+        var urls = ['https://theurbanist.org'];
+        if (urls) {
+            urls.forEach(function (value) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    const res = yield axios_1.default.get(value);
+                    if (res.status != 200) {
+                        const url = 'https://hooks.slack.com/services/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';
+                        const res = yield axios_1.default.post(url, {
+                            channel: '#test',
+                            text: `${value} is down`
+                        }, { headers: { authorization: `Bearer ${slackToken}` } });
+                    }
+                });
+            });
+        }
     });
 }
 run();
